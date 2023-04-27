d1f60813d601fa7262641bbeac8cdc51408e0960
Priyabrata Maiti <priyabrata.maiti@wisesoftwaresolutions.com>
2023-04-28 01:38:58 +0530

    modf: app

--- FrontEnd/src/Services/StandardConst.jsx
+++ FrontEnd/src/Services/StandardConst.jsx
@@ -2,9 +2,10 @@ import forgotPasswordSvg from "../asset/Forgot-password-cuate.svg";
 import userLoginSvg from "../asset/Forgot-password-cuate.svg";
 
 export const StandardConst = {
-  apiBaseUrl: "http://localhost:3001",
-  images: "http://localhost:3001/images/",
-  uploadImages: "http://localhost:3001/uploads/",
+  apiBaseUrl: "http://54.196.184.46:3001",
+  //apiBaseUrl: "http://localhost:3001",
+  images: "http://54.196.184.46:3001/images/",
+  uploadImages: "http://54.196.184.46:3001/uploads/",
   employeeId: "1",
   forgotPassword: forgotPasswordSvg,
   userLoginSvg: userLoginSvg,
