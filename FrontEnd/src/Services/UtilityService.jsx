d58fd257401535b8f2a3f008ef8bc847a80f0c43
Priyabrata Maiti <priyabrata.maiti@wisesoftwaresolutions.com>
2023-05-01 09:10:52 +0530

    modf: delete files all

--- FrontEnd/src/Services/UtilityService.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const dataClone = (data) => JSON.parse(JSON.stringify(data));
-export const contentSearch = (data, search) =>
-  (data ?? "")
-    .toString()
-    .toLowerCase()
-    .indexOf((search ?? "").toString().toLowerCase()) > -1;
